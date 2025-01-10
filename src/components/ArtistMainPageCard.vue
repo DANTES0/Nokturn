10baa9e0e995c9b194602bc448352fc9d65e7009
DANTES0 <kzstar18rus@gmail.com>
2025-01-10 18:09:59 +0400

    ArtistMainPageCard html coding completed

---
+++ src/components/ArtistMainPageCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="w-[360px] h-[320px] shadow-card rounded-3xl hover:scale-110 relative flex flex-col items-center"
+  >
+    <div
+      class="w-full h-[55%] bg-black flex rounded-tl-2xl rounded-tr-2xl first:rounded-tl-2xl last:rounded-tr-2xl bro"
+    >
+      <img class="object-cover w-[120px]" src="../assets/images/test1.jpg" />
+      <img class="object-cover w-[120px]" src="../assets/images/test2.jpg" />
+      <img class="object-cover w-[120px]" src="../assets/images/test1.jpg" />
+    </div>
+    <img
+      class="absolute w-[100px] h-[100px] rounded-full bg-transparent shadow-cardImage top-[35%] object-cover"
+      src="../assets/images/test3.png"
+    />
+    <div class="mt-[40px] w-full flex flex-col gap-2">
+      <span class="ml-[20px] text-[20px] font-medium">Имя Фамилия</span>
+      <div class="ml-[20px] font-light">
+        Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст Текст
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bro img:first-child {
+  border-top-left-radius: 16px;
+}
+
+.bro img:last-child {
+  border-top-right-radius: 16px;
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ArtistMainPageCard from '@/components/ArtistMainPageCard.vue'
 import AuctionCard from '@/components/AuctionCard.vue'
 import IconLeftArrow from '@/components/icons/IconLeftArrow.vue'
 import IconRightArrow from '@/components/icons/IconRightArrow.vue'
@@ -44,12 +45,30 @@ import { RouterLink } from 'vue-router'
     <div class="flex flex-wrap w-[90%] justify-center gap-x-14 gap-y-8">
       <AuctionCard v-for="i in 8" :key="i" />
     </div>
-    <div class="mt-[30px] mb-[30px] flex w-[90%] justify-between">
+    <div class="mt-[30px] flex w-[90%] justify-between">
       <label class="text-[24px]"> Художники </label>
 
       <RouterLink class="font-light flex items-center justify-center" to="/artists">
         Посмотреть всех художников</RouterLink
       >
+    </div>
+
+    <div
+      class="w-[90%] h-[400px] bg-white mt-[30px] shadow-container rounded-[32px] relative max-h-[80vh] flex items-center justify-between mb-[100px]"
+    >
+      <div
+        class="w-12 h-12 bg-[#F5F5F5] rounded-full ml-[30px] flex items-center justify-center hover:scale-110 cursor-pointer shadow-cardImage"
+      >
+        <IconLeftArrow />
+      </div>
+
+      <ArtistMainPageCard v-for="i in 3" :key="i" />
+
+      <div
+        class="w-12 h-12 bg-[#F5F5F5] rounded-full mr-[30px] flex items-center justify-center hover:scale-110 cursor-pointer shadow-cardImage"
+      >
+        <IconRightArrow />
+      </div>
     </div>
   </div>
 </template>
