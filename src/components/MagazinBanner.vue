e7a27d81868ebcbdf513a4b7a5a43755c3adcf96
DANTES0 <kzstar18rus@gmail.com>
2025-01-10 15:25:30 +0400

    MagazinBanner html coding completed

---
+++ src/components/MagazinBanner.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import IconLeftArrow from '@/components/icons/IconLeftArrow.vue'
+import IconRightArrow from '@/components/icons/IconRightArrow.vue'
+</script>
+
+<template>
+  <div
+    class="w-[90%] h-[500px] bg-white mt-[30px] shadow-container rounded-[32px] relative max-h-[80vh]"
+  >
+    <div class="trapezoid">
+      <div class="w-[300px] h-[100px] ml-[30px] mt-[30px] relative">
+        <div class="absolute w-full h-full blur bg-black/40"></div>
+        <p class="pl-[20px] pt-[10px] relative">Торги художника</p>
+        <p class="text-right pr-[30px] font-['InterItalic'] font-light text-[28px] relative">
+          Безымянный
+        </p>
+      </div>
+      <div
+        class="w-12 h-12 bg-black/25 rounded-full ml-[30px] flex items-center justify-center hover:scale-110 cursor-pointer"
+      >
+        <IconLeftArrow />
+      </div>
+      <div
+        class="w-[300px] h-[100px] ml-[30px] mb-[30px] relative flex items-center justify-center"
+      >
+        <div class="absolute w-full h-full bg-black/40 blur"></div>
+        <p class="relative text-center text-[42px] font-bold font-[InterItalic]">20 Октября</p>
+      </div>
+    </div>
+    <div class="trapezoid2">
+      <div
+        class="w-[300px] h-[100px] mr-[30px] mt-[30px] relative flex items-center justify-center"
+      >
+        <div class="absolute w-full h-full bg-black/40 blur"></div>
+        <p class="relative text-center text-[42px] font-bold font-[InterItalic]">20 Октября</p>
+      </div>
+      <div
+        class="w-12 h-12 bg-black/25 rounded-full mr-[30px] flex items-center justify-center hover:scale-110 cursor-pointer"
+      >
+        <IconRightArrow />
+      </div>
+      <div class="w-[300px] h-[100px] mr-[30px] mb-[30px] relative">
+        <div class="absolute w-full h-full blur bg-black/40"></div>
+        <p class="pr-[20px] pt-[10px] relative text-right">Торги художника</p>
+        <p class="pl-[30px] font-['InterItalic'] font-light text-[28px] relative">Безымянный</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.trapezoid {
+  position: absolute;
+
+  width: 60%;
+  height: 90%;
+  margin-left: 30px;
+  margin-top: 25px;
+  background: url('../assets/images/test1.jpg');
+  background-size: 100% auto;
+  border-top-left-radius: 16px;
+  border-bottom-left-radius: 16px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  font-size: 24px;
+  font-weight: bold;
+  clip-path: polygon(0% 0%, 100% 0%, 70% 100%, 0% 100%);
+}
+.trapezoid2 {
+  position: absolute;
+  right: 0;
+  width: 60%;
+  height: 90%;
+  margin-top: 25px;
+  margin-right: 30px;
+  border-bottom-right-radius: 16px;
+  border-top-right-radius: 16px;
+  background: url('../assets/images/test2.jpg');
+  background-size: 100% auto;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: end;
+  font-size: 24px;
+  font-weight: bold;
+  clip-path: polygon(30% 0%, 100% 0%, 100% 100%, 0% 100%);
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,66 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MagazinBanner from '@/components/MagazinBanner.vue'
+import { RouterLink } from 'vue-router'
+</script>
 <template>
-  <div
-    class="w-[90%] h-[500px] bg-white mt-[30px] shadow-container rounded-2xl relative max-w-[1382px] max-h-[80vh]"
-  >
-    <div class="trapezoid">
-      <p>Торги художника<br />Name</p>
+  <div class="flex items-center flex-col w-full max-w-[1728px]">
+    <MagazinBanner />
+
+    <div class="mt-[30px] flex w-[90%] justify-between">
+      <label class="text-[24px]"> Ближайшие торги </label>
+
+      <RouterLink class="font-light flex items-center justify-center" to="/auctions">
+        Посмотреть все торги</RouterLink
+      >
     </div>
-    <div class="trapezoid2">
-      <p>Торги художника<br />Name</p>
-    </div>
+    <div
+      class="w-[90%] h-[400px] bg-white mt-[30px] shadow-container rounded-[32px] relative max-h-[80vh] mb-[100px]"
+    ></div>
   </div>
 </template>
 
-<style scoped>
-.trapezoid {
-  position: absolute;
-
-  width: 60%; /* Ширина трапеции */
-  height: 90%; /* Высота трапеции */
-  margin-left: 30px;
-  margin-top: 25px;
-  background: url('../assets/images/test1.jpg');
-  background-size: 100% 100%; /* Цвет фона */
-  border-top-left-radius: 16px;
-  border-bottom-left-radius: 16px;
-  color: white; /* Цвет текста */
-  display: flex; /* Центрирование текста */
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 18px;
-  font-weight: bold;
-  clip-path: polygon(0% 0%, 100% 0%, 70% 100%, 0% 100%);
-  /* Параметры clip-path:
-     - 10% 0% — верхний левый угол сдвинут внутрь на 10%
-     - 90% 0% — верхний правый угол сдвинут внутрь на 10%
-     - 100% 100% — правый нижний угол
-     - 0% 100% — левый нижний угол */
-}
-.trapezoid2 {
-  position: absolute;
-  right: 0;
-  width: 60%; /* Ширина трапеции */
-  height: 90%; /* Высота трапеции */
-  margin-top: 25px;
-  margin-right: 30px;
-  border-bottom-right-radius: 16px;
-  border-top-right-radius: 16px;
-  background: url('../assets/images/test2.jpg');
-  background-size: 100% 100%; /* Цвет фона */
-  color: white; /* Цвет текста */
-  display: flex; /* Центрирование текста */
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 18px;
-  font-weight: bold;
-  clip-path: polygon(30% 0%, 100% 0%, 100% 100%, 0% 100%);
-  /* Параметры clip-path:
-     - 10% 0% — верхний левый угол сдвинут внутрь на 10%
-     - 90% 0% — верхний правый угол сдвинут внутрь на 10%
-     - 100% 100% — правый нижний угол
-     - 0% 100% — левый нижний угол */
-}
-</style>
+<style scoped></style>
