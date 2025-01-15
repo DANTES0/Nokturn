9a8429ad7b8a86306c2ead124759afe3586f9ec0
DANTES0 <kzstar18rus@gmail.com>
2025-01-15 18:23:10 +0400

    Страница художники сверстана(почти)

---
+++ src/components/ArtistPageComponents/ArtistCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-white shadow-card p-[30px] rounded-2xl flex w-full">
+    <div class="flex items-center">
+      <img class="w-24 h-24 rounded-full shadow-card" src="../../assets/images/test3.png" />
+      <div class="flex flex-col ml-[20px] max-w-[300px] gap-3 border-r border-black pr-[10px]">
+        <span class="mt-[10px] text-[18px]">Имя Фамилия</span>
+        <span class="mb-[10px] text-[14px] font-light"
+          >Текст Текст Текст Текст Текст Текст Текст Текст Текст</span
+        >
+      </div>
+    </div>
+    <div class="flex w-full flex-1 ml-[20px] gap-4 items-center justify-center">
+      <img
+        v-for="i in 5"
+        :key="i"
+        class="w-[20%] h-auto rounded-2xl max-w-[160px] max-h-[160px]"
+        src="../../assets/images/test5.jpg"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/ArtistPage.vue
+++ src/pages/ArtistPage.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ArtistCard from '@/components/ArtistPageComponents/ArtistCard.vue'
+import IconSearch from '@/components/icons/IconSearch.vue'
+</script>
 <template>
-  <div>Художники</div>
+  <div class="w-[90%] mt-[30px]">
+    <div class="relative mr-auto">
+      <input
+        class="border border-black w-[400px] h-[30px] rounded-bl-lg rounded-tr-lg pl-4 pr-[40px]"
+        type="text"
+        placeholder="Поиск"
+      />
+      <IconSearch class="absolute left-[360px] top-[4px]" />
+    </div>
+  </div>
+  <div
+    class="w-[90%] bg-white mt-[30px] shadow-container rounded-[32px] relative flex mb-[30px] gap-[30px] flex-col px-[30px] py-[30px]"
+  >
+    <ArtistCard v-for="i in 4" :key="i" />
+  </div>
 </template>
 <style scoped></style>
