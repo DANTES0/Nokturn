7f91c860be703dc9e892f45870f640efd9c4da9f
DANTES0 <kzstar18rus@gmail.com>
2025-01-23 19:25:05 +0400

    Доверстал страницу чата

---
+++ src/components/ChatPageComponents/ChatMessenger.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex">
+    <div class="">
+      <img
+        src="../../assets/images/test3.png"
+        class="rounded-full shadow-cardImage w-[3.8vw] aspect-square"
+      />
+    </div>
+    <div class="flex flex-col flex-1 justify-around ml-4">
+      <div class="flex w-full justify-between items-center">
+        <div class="font-medium text-[1vw]">Петриков Кирилл</div>
+        <div class="font-extralight text-[0.7vw]">10:23</div>
+      </div>
+      <div class="w-full text-[0.8vw] pr-[40px] text-justify">
+        Текст со специальной информацией Текст со специальной информациейТекст со специальной
+        информацией Текст со специальной информацией
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import ChatMessenger from '@/components/ChatPageComponents/ChatMessenger.vue'
 import ChatUserCard from '@/components/ChatPageComponents/ChatUserCard.vue'
+import IconPaperClip from '@/components/icons/IconPaperClip.vue'
+import MyButton from '@/UX/MyButton.vue'
 </script>
 
 <template>
@@ -9,7 +12,21 @@ import ChatUserCard from '@/components/ChatPageComponents/ChatUserCard.vue'
     >
       <ChatUserCard v-for="i in 10" :key="i" />
     </div>
-    <div class="bg-white w-1/2 h-[85vh] shadow-container rounded-lg min-h-[400px]"></div>
+    <div class="bg-white w-1/2 h-[85vh] shadow-container rounded-lg">
+      <div class="flex flex-col gap-6 overflow-y-scroll h-[72vh] w-full p-[20px]">
+        <ChatMessenger v-for="i in 10" :key="i" />
+      </div>
+      <div class="w-full h-[15vh] p-[20px] flex justify-between items-center">
+        <div class="w-[70%] relative">
+          <input
+            class="w-full border-b border-black h-[40px] pl-[10px] outline-none pr-[40px] text-[1vw]"
+            placeholder="Написать сообщение"
+          />
+          <IconPaperClip class="absolute right-0 top-[6px] cursor-pointer hover:text-[#444444]" />
+        </div>
+        <MyButton title="Отправить" class="text-[0.8vw]" />
+      </div>
+    </div>
   </div>
 </template>
 
