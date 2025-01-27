7b4c8afdb944c5304aa2e50c8a4589aa40700cc0
DANTES0 <kzstar18rus@gmail.com>
2025-01-27 16:30:21 +0400

    Улучшил юзабилити для страницы аукциона

--- src/pages/AuctionPage.vue
+++ src/pages/AuctionPage.vue
@@ -23,50 +23,56 @@ const activeButtonFilter = ref(false)
     >
       <IconRightArrow />
     </div>
-    <div
-      v-if="activeButtonFilter || !isMobile"
-      class="max-w-[350px] bg-[#FAFAFA] mt-[30px] shadow-container rounded-[32px] mb-[30px] top-[30px] flex flex-col overflow-y-scroll"
-      :class="[
-        { 'fixed w-full z-10  h-[75vh] left-0': isMobile },
-        { 'sticky w-[25%] h-[90vh]': !isMobile },
-      ]"
-    >
-      <div class="flex flex-col w-full px-[30px] flex-1">
-        <div class="flex items-center mt-[20px] justify-between">
-          <label class="text-[20px]">Фильтры</label>
-          <div
-            v-if="isMobile"
-            class="bg-[#FAFAFA] w-10 h-10 rounded-full shadow-container absolute right-3 flex items-center justify-center"
-          >
-            <IconLeftArrow class="w-8 h-8" @click="activeButtonFilter = false" />
+    <Transition name="slide-right">
+      <div
+        v-if="activeButtonFilter || !isMobile"
+        class="max-w-[350px] bg-[#FAFAFA] mt-[30px] shadow-container mb-[30px] top-[30px] flex flex-col overflow-y-scroll"
+        :class="[
+          { 'fixed w-full z-10  h-[75vh] left-0 rounded-tr-[32px] rounded-br-[32px]': isMobile },
+          { 'sticky w-[25%] h-[90vh] rounded-[32px]': !isMobile },
+        ]"
+      >
+        <div class="flex flex-col w-full px-[30px] flex-1">
+          <div class="flex items-center mt-[20px] justify-between">
+            <label class="text-[20px]">Фильтры</label>
+            <div
+              v-if="isMobile"
+              class="bg-[#FAFAFA] w-10 h-10 rounded-full shadow-container absolute right-3 flex items-center justify-center"
+            >
+              <IconLeftArrow class="w-8 h-8" @click="activeButtonFilter = false" />
+            </div>
+          </div>
+          <MyInput title="Поиск" placeholder="Арбуз" class="mt-[16px]" />
+          <MySelectionInput
+            title="Категория"
+            placeholder="Физическое искусство"
+            class="mt-[30px]"
+          />
+          <div class="mt-[20px]">Начальная цена</div>
+          <div class="flex gap-5">
+            <MyInput title="От" placeholder="100" class="mt-[14px]" />
+            <MyInput title="До" placeholder="10000" class="mt-[14px]" />
+          </div>
+          <div class="mt-[10px]">Текущая цена</div>
+          <div class="flex gap-5">
+            <MyInput title="От" placeholder="100" class="mt-[14px]" />
+            <MyInput title="До" placeholder="10000" class="mt-[14px]" />
+          </div>
+          <div class="flex mt-[20px] gap-7 w-full">
+            <MyCheckBox class="w-6 h-6" />
+            <label class="">Показывать текущие</label>
+          </div>
+          <div class="mt-[10px]">Размер картины</div>
+          <div class="flex gap-5">
+            <MyInput title="Длина" placeholder="100" class="mt-[14px]" />
+            <MyInput title="Высота" placeholder="10000" class="mt-[14px]" />
+          </div>
+          <div class="w-full flex items-center justify-center mt-auto mb-[30px]">
+            <MyButton title="Применить" />
           </div>
         </div>
-        <MyInput title="Поиск" placeholder="Арбуз" class="mt-[16px]" />
-        <MySelectionInput title="Категория" placeholder="Физическое искусство" class="mt-[30px]" />
-        <div class="mt-[20px]">Начальная цена</div>
-        <div class="flex gap-5">
-          <MyInput title="От" placeholder="100" class="mt-[14px]" />
-          <MyInput title="До" placeholder="10000" class="mt-[14px]" />
-        </div>
-        <div class="mt-[10px]">Текущая цена</div>
-        <div class="flex gap-5">
-          <MyInput title="От" placeholder="100" class="mt-[14px]" />
-          <MyInput title="До" placeholder="10000" class="mt-[14px]" />
-        </div>
-        <div class="flex mt-[20px] gap-7 w-full">
-          <MyCheckBox class="w-6 h-6" />
-          <label class="">Показывать текущие</label>
-        </div>
-        <div class="mt-[10px]">Размер картины</div>
-        <div class="flex gap-5">
-          <MyInput title="Длина" placeholder="100" class="mt-[14px]" />
-          <MyInput title="Высота" placeholder="10000" class="mt-[14px]" />
-        </div>
-        <div class="w-full flex items-center justify-center mt-auto mb-[30px]">
-          <MyButton title="Применить" />
-        </div>
       </div>
-    </div>
+    </Transition>
     <div
       class="w-full bg-[#FAFAFA] mt-[30px] shadow-container rounded-[32px] relative mb-[30px] auc-card py-[30px] px-[30px]"
     >
@@ -81,5 +87,32 @@ const activeButtonFilter = ref(false)
   row-gap: 2em;
   column-gap: 1em;
   justify-items: center;
+}
+
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition:
+    transform 0.3s ease,
+    opacity 0.3s ease;
+}
+
+.slide-right-enter-from {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
+.slide-right-enter-to {
+  transform: translateX(0);
+  opacity: 1;
+}
+
+.slide-right-leave-from {
+  transform: translateX(0);
+  opacity: 1;
+}
+
+.slide-right-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
 }
 </style>
