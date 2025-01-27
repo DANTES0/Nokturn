addc63144f70f9c49feef21021d3e0c020c343bc
DANTES0 <kzstar18rus@gmail.com>
2025-01-27 17:14:53 +0400

    Кароч адаптирова страницу с художниками, но надо подумать

--- src/components/ArtistPageComponents/ArtistCard.vue
+++ src/components/ArtistPageComponents/ArtistCard.vue
@@ -1,21 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useScreenWidth } from '@/assets/scripts/ScriptWindowSize'
+import { onMounted, ref, watch } from 'vue'
+
+const isMobile = useScreenWidth(1300)
+
+const sizeArrayImage = ref(5)
+
+watch(isMobile, () => {
+  if (isMobile.value) {
+    sizeArrayImage.value = 3
+  } else sizeArrayImage.value = 5
+})
+
+onMounted(() => {
+  if (isMobile.value) {
+    sizeArrayImage.value = 3
+  }
+})
+</script>
 
 <template>
   <div class="bg-white shadow-card p-[30px] rounded-2xl flex w-full">
-    <div class="flex items-center">
+    <div class="flex items-center mobile:flex-row flex-col">
       <img class="w-24 h-24 rounded-full shadow-card" src="../../assets/images/test3.png" />
-      <div class="flex flex-col ml-[20px] max-w-[300px] gap-3 border-r border-black pr-[10px]">
-        <span class="mt-[10px] text-[18px]">Имя Фамилия</span>
-        <span class="mb-[10px] text-[14px] font-light"
+      <div
+        class="flex flex-col mobile:ml-[20px] mobile:max-w-[300px] max-w-[200px] gap-3 mobile:border-r mobile:border-black mobile:pr-[10px]"
+      >
+        <span class="mt-[10px] text-[18px] mobile:text-start text-center">Имя Фамилия</span>
+        <span class="mb-[10px] text-[14px] font-light mobile:text-start text-center"
           >Текст Текст Текст Текст Текст Текст Текст Текст Текст</span
         >
       </div>
     </div>
-    <div class="flex w-full flex-1 ml-[20px] gap-4 items-center justify-center">
+    <div class="flex w-full ml-[20px] gap-4 items-center justify-center mobile:flex-row flex-col">
       <img
-        v-for="i in 5"
+        v-for="i in sizeArrayImage"
         :key="i"
-        class="w-[20%] h-auto rounded-2xl max-w-[160px] max-h-[160px]"
+        class="mobile:w-[11vw] w-[100px] h-auto rounded-2xl max-w-[160px] max-h-[160px]"
         src="../../assets/images/test5.jpg"
       />
     </div>
--- src/pages/ArtistPage.vue
+++ src/pages/ArtistPage.vue
@@ -5,17 +5,17 @@ import IconSearch from '@/components/icons/IconSearch.vue'
 </script>
 <template>
   <div class="w-[90%] mt-[30px]">
-    <div class="relative mr-auto">
+    <div class="relative mr-auto w-full max-w-[400px]">
       <input
-        class="border border-black w-[400px] h-[30px] rounded-bl-lg rounded-tr-lg pl-4 pr-[40px]"
+        class="border border-black w-full h-[30px] rounded-bl-lg rounded-tr-lg pl-4 pr-[40px] max-w-[400px]"
         type="text"
         placeholder="Поиск"
       />
-      <IconSearch class="absolute left-[360px] top-[4px]" />
+      <IconSearch class="absolute right-[8px] top-[4px]" />
     </div>
   </div>
   <div
-    class="w-[90%] bg-white mt-[30px] shadow-container rounded-[32px] relative flex mb-[30px] gap-[30px] flex-col px-[30px] py-[30px]"
+    class="w-[90%] bg-white mt-[30px] shadow-container rounded-[32px] relative flex mb-[30px] gap-[30px] flex-col px-[30px] py-[30px] text-center items-center"
   >
     <ArtistCard v-for="i in 4" :key="i" />
   </div>
