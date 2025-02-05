68b55c8db9e2afca3186db3d8acf829fb9696622
DANTES0 <kzstar18rus@gmail.com>
2025-02-05 19:27:59 +0400

    Добавил запрос для регистрации

--- src/UX/MySecondInput.vue
+++ src/UX/MySecondInput.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+
 interface Props {
   title: string
   placeholder: string
@@ -8,11 +10,22 @@ const props = withDefaults(defineProps<Props>(), {
   title: 'Название',
   placeholder: 'Поиск',
 })
+
+const inputModel = ref('')
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+watch(inputModel, (newValue) => {
+  emit('update:modelValue', newValue)
+})
 </script>
 <template>
   <div>
     <div class="ml-[10px] text-[14px] mb-[8px]">{{ props.title }}</div>
     <input
+      v-model="inputModel"
       type="text"
       class="border border-black rounded-tl-lg rounded-br-lg w-full py-[4px] text-[1vw] pl-4 focus:outline-black focus:outline focus:outline-1"
       :placeholder="props.placeholder"
--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -3,6 +3,33 @@ import WelcomCard from '@/components/WelcomCard.vue'
 import MyButton from '@/UX/MyButton.vue'
 import MyCheckBox from '@/UX/MyCheckBox.vue'
 import MySecondInput from '@/UX/MySecondInput.vue'
+import { ref } from 'vue'
+
+async function fetchData() {
+  const objectData = {
+    firstname: loginModel.value,
+    mail: emailModel.value,
+    password: passwordModel.value,
+    birthday_date: new Date(birthdayModel.value),
+  }
+  console.log(objectData)
+  if (!loginModel.value || !emailModel.value || !passwordModel.value || !birthdayModel.value) {
+    console.log('Не все данные')
+    return
+  }
+  await fetch('http://localhost:3000/api/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(objectData),
+  })
+}
+
+const emailModel = ref('')
+const loginModel = ref('')
+const passwordModel = ref('')
+const birthdayModel = ref('')
 </script>
 
 <template>
@@ -12,18 +39,22 @@ import MySecondInput from '@/UX/MySecondInput.vue'
       class="bg-[rgba(255,255,255,0.8)] h-[90vh] w-[35vw] flex flex-col p-[30px] justify-between"
     >
       <div class="w-full text-center font-medium text-[20px]">Регистрация</div>
-      <MySecondInput placeholder="example@mail.ru" title="Email" />
-      <MySecondInput placeholder="Имя/Псевдноним" title="Имя/Псевдноним" />
-      <MySecondInput placeholder="Пароль" title="Пароль" />
+      <MySecondInput placeholder="example@mail.ru" title="Email" v-model="emailModel" />
+      <MySecondInput placeholder="Имя/Псевдноним" title="Имя/Псевдноним" v-model="loginModel" />
+      <MySecondInput placeholder="Пароль" title="Пароль" v-model="passwordModel" />
       <MySecondInput placeholder="Повторите пароль" title="Повторите пароль" />
-      <MySecondInput placeholder="11-11-1111" title="Введите вашу дату рождения" />
+      <MySecondInput
+        placeholder="11-11-1111"
+        title="Введите вашу дату рождения"
+        v-model="birthdayModel"
+      />
 
       <div class="flex gap-4 mt-[14px]">
         <MyCheckBox />
         <div class="text-[1vw]">Подтвердите условие участия в аукционах</div>
       </div>
       <div class="flex gap-6 items-center">
-        <MyButton title="Зарегистрировать" />
+        <MyButton @click="fetchData" title="Зарегистрировать" />
         <div class="w-[150px] text-[0.6vw]">
           У вас уже есть аккаунт?
           <RouterLink to="/auth"
